e8c3ffd7a44023c6fefd64740b0da9bd95499ad4
Juliana Girotto <ads.jjgirotto@gmail.com>
2024-09-03 19:35:24 -0300

    Product and Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/repositories/CategoryRepository.java
+++ src/main/java/com/educandoweb/course/repositories/CategoryRepository.java
@@ -1,7 +1,6 @@
 package com.educandoweb.course.repositories;
 
 import com.educandoweb.course.entities.Category;
-import com.educandoweb.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
--- src/main/java/com/educandoweb/course/repositories/ProductRepository.java
+++ src/main/java/com/educandoweb/course/repositories/ProductRepository.java
@@ -1,6 +1,5 @@
 package com.educandoweb.course.repositories;
 
-import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
