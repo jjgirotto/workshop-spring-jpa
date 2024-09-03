2b36eac3f75c888706521e6de590dee8b4b4c0b9
Juliana Girotto <ads.jjgirotto@gmail.com>
2024-09-03 16:15:27 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -6,13 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//camada de recurso são camadas controladoras (controllers)
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/users") //recurso web da entidade user com o caminho /users
 public class UserResource {
 
+    //método para acessar os users
+    //o response é do spring e retorna respostas web
     @GetMapping
     public ResponseEntity<User> findAll() {
         User u = new User(1L, "maria", "maria@gmail.com", "99999", "kkk323");
         return ResponseEntity.ok().body(u);
+        //ok para retornar com sucesso e body para retornar o corpo do u
     }
 }
